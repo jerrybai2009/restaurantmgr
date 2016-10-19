5606e2462e0888e8f59b76641cdb1c55db01211d
jbai <jbai@linkedin.com>
2016-10-19 13:08:22 -0700

    test web chat authentication parameters

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,19 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result WechatAuthenticate() {
+
+        String signature = request().getQueryString("signature");
+        String timestamp = request().getQueryString("timestamp");
+        String nonce = request().getQueryString("nonce");
+        String echostr = request().getQueryString("echostr");
+        String token = request().getQueryString("token");
+        Logger.info(signature + timestamp + nonce + echostr + token);
+
         return ok(index.render("Your new application is ready."));
     }
 

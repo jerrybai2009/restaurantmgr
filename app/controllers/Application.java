7260357d077a04b5f1c6aa054673c9c20a5d88b5
jbai <jbai@linkedin.com>
2016-10-24 00:30:25 -0700

    add paypal function

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,149 @@
 package controllers;
 
+import com.braintreegateway.BraintreeGateway;
+import com.braintreegateway.Transaction;
+import com.braintreegateway.TransactionRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.lang3.StringUtils;
 import play.*;
 import play.mvc.*;
 import utils.WechatSignature;
 
 import views.html.*;
 
+import java.math.BigDecimal;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+
 public class Application extends Controller {
 
     private static final String TITLE = "美食餐厅";
     public static Result index() {
 
+
+        String accessToken = "TXCaVc_eEh_wZEkBQAxxgEyR3rGvt0ZlcITbn4YpUix6-usyzYFzn6aYmj4U14Rj2QN_NV3SxsaBT2yAnuA86U_8hqFMfvOP--yZfsFe1TUPLLfAEAYUU";
+        String ticket = "kgt8ON7yVITDhtdwci0qeeeauWc5eehDcjaNxRK2PnMfvNbJcJZ8e3uM-sBBF1wqKMRgzCMJIQ7e252Ng6ocDA";
+        String noncestr = UUID.randomUUID().toString().replace("-", "").substring(0, 16);
+        String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+        String url="https://restaurantmgr.herokuapp.com";
+        String[] ArrTmp = {"jsapi_ticket","timestamp","nonce","url"};
+        Arrays.sort(ArrTmp);
+        StringBuffer sf = new StringBuffer();
+        for(int i=0;i<ArrTmp.length;i++){
+            sf.append(ArrTmp[i]);
+        }
+        String signature =SHA1(sf.toString());
+        //Logger.info(timestamp);
+        //Logger.info(signature);
+        //Logger.info(noncestr);
+
+
         return ok(index.render(TITLE));
+    }
+
+    public static Result paypalToken()
+    {
+        BraintreeGateway gateway = new BraintreeGateway("access_token$sandbox$28fhqgwypvd7gnsn$2d3066d4e891752ed2b5ba4dc425a9d4");
+
+        return ok(gateway.clientToken().generate());
+    }
+
+    public static Result paypalCheckout()
+    {
+        String nonce = null;
+        Map<String, String[]> params = request().body().asFormUrlEncoded();
+        if (params == null || params.size() == 0)
+        {
+            return ok("Empty post body");
+        }
+
+        if (params.containsKey("payment_method_nonce")) {
+            String[] nonceArray = params.get("payment_method_nonce");
+            if (nonceArray != null && nonceArray.length > 0)
+            {
+                nonce = nonceArray[0];
+            }
+        }
+        String amountStr = null;
+        if (params.containsKey("amount")) {
+            String[] amountArray = params.get("amount");
+            if (amountArray != null && amountArray.length > 0)
+            {
+                amountStr = amountArray[0];
+            }
+        }
+        if (StringUtils.isBlank(nonce) || StringUtils.isBlank(amountStr)) {
+            return ok("wrong parameter");
+        }
+
+        BigDecimal amount = new BigDecimal(amountStr);
+
+        BraintreeGateway gateway = new BraintreeGateway("access_token$sandbox$28fhqgwypvd7gnsn$2d3066d4e891752ed2b5ba4dc425a9d4");
+
+        TransactionRequest request = new TransactionRequest().
+                amount(amount).
+                merchantAccountId("USD").
+                paymentMethodNonce(nonce).
+                //orderId("Mapped to PayPal Invoice Number").
+                // descriptor().
+                //name("Inc").
+                //done().
+
+        shippingAddress()
+        .firstName("Jen")
+            .lastName("Smith")
+            .company("BigWhite Dev")
+            .streetAddress("1 E 1st St")
+            .extendedAddress("Suite 403")
+            .locality("Bartlett")
+            .region("IL")
+            .postalCode("60103")
+            .countryCodeAlpha2("US")
+            .done().options().
+                paypal().
+                customField("PayPal custom field").
+                description("Online food").
+                done().
+        storeInVaultOnSuccess(true).
+                done();
+
+        com.braintreegateway.Result<Transaction> saleResult = gateway.transaction().sale(request);
+
+        if (saleResult.isSuccess()) {
+            Transaction transaction = saleResult.getTarget();
+
+            Logger.info("Success ID: " + transaction.getId());
+            return ok(transaction.getId());
+        } else {
+            Logger.error("Message: " + saleResult.getMessage());
+        }
+        return badRequest("Transaction failed");
+    }
+
+    public static String SHA1(String decript) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("SHA-1");
+            digest.update(decript.getBytes());
+            byte messageDigest[] = digest.digest();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            // 字节数组转换为 十六进制 数
+            for (int i = 0; i < messageDigest.length; i++) {
+                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexString.append(0);
+                }
+                hexString.append(shaHex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
     public static Result WechatAuthenticate() {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-
+/*
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wxb1a10559fba39dd1', // 必填，公众号的唯一标识
@@ -19,3 +19,62 @@ wx.onMenuShareTimeline({
         // 用户取消分享后执行的回调函数
     }
 });
+*/
+
+// Fetch the button you are using to initiate the PayPal flow
+var paypalButton = document.getElementById('paypal-button');
+
+var tokenUrl = 'api/v1/token';
+$.get(tokenUrl, function(data) {
+    braintree.client.create({
+        authorization: data
+    }, function (clientErr, clientInstance) {
+        // Create PayPal component
+        braintree.paypal.create({
+            client: clientInstance
+        }, function (err, paypalInstance) {
+            paypalButton.addEventListener('click', function () {
+                // Tokenize here!
+                paypalInstance.tokenize({
+                    flow: 'checkout', // Required
+                    amount: 10.00, // Required
+                    currency: 'USD', // Required
+                    locale: 'en_US',
+                    enableShippingAddress: true,
+                    shippingAddressEditable: false,
+                    shippingAddressOverride: {
+                        recipientName: 'Testing',
+                        line1: '1234 Main St.',
+                        line2: 'Unit 1',
+                        city: 'Mountainview',
+                        countryCode: 'US',
+                        postalCode: '94039',
+                        state: 'CA',
+                        phone: '123.456.7890'
+                    }
+                }, function (err, tokenizationPayload) {
+                    // Tokenization complete
+                    // Send tokenizationPayload.nonce to server
+                    console.log(tokenizationPayload.nonce);
+                    var checkoutUrl = 'api/v1/checkout';
+                    $.ajax({
+                        url: checkoutUrl,
+                        method: 'POST',
+                        data: {
+                            payment_method_nonce: tokenizationPayload.nonce,
+                            amount: 10.00
+                        }
+                    }).done(function(data, txt, xhr){
+                        alert("Order completed. Transaction id is " + data)
+                    }).fail(function(xhr, txt, error){
+                        alert("Order failed.");
+                    })
+                });
+            });
+        });
+    });
+});
+// Create a Client component
+
+
+

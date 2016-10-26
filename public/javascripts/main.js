e2ab44c7e9891bf3d7dd8ea9ab27ed14fe131ee8
jbai <jbai@linkedin.com>
2016-10-25 23:41:27 -0700

    update the signature

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,6 @@
 
 
+/*
 var signatureUrl = 'api/v1/signature?url=' + location.href.split('#')[0];
 $.get(signatureUrl, function(data) {
     if (data && data.status === 'ok') {
@@ -28,14 +29,14 @@ $.get(signatureUrl, function(data) {
     }
 
 });
+*/
 
-/*
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wxb1a10559fba39dd1', // 必填，公众号的唯一标识
     timestamp: 1477460337, // 必填，生成签名的时间戳
     nonceStr: '8167ab44137343a2', // 必填，生成签名的随机串
-    signature: '2e0e9153f9c36f1d1935cdca595ce7901facf609',// 必填，签名，见附录1
+    signature: '48ac7f844e1834bb03587d850f6750843b5ab0e4',// 必填，签名，见附录1
     jsApiList: ["chooseImage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 });
 
@@ -52,7 +53,7 @@ wx.onMenuShareTimeline({
     }
 });
 
-*/
+
 // Fetch the button you are using to initiate the PayPal flow
 var paypalButton = document.getElementById('paypal-button');
 
